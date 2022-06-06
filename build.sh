d6ea5573917555cec78e65061455692f0152c0be
vinhjaxt <ngoc.lo.dang@gmail.com>
2022-06-06 16:08:37 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+HOST_NAME="nessus"
+
+docker build -t "${HOST_NAME}:latest" "${DIR}/build"
+if [ $? -eq 0 ]; then
+  echo 'Build done'
+else
+  echo 'Build failed'
+  exit 1
+fi
